ab692b455ceccee268791a5c1d911f4403e8be0c
koma <maks@komaval.com>
2014-07-15 12:29:51 +0300

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+var xmlrpc  = require('xmlrpc');
+var Promise = require('bluebird');
+
+function promisify(methodName) {
+  var method = xmlrpc[methodName];
+  
+  return function() {
+    var client = method.apply(xmlrpc, arguments);
+    client.methodCall = Promise.promisify(client.methodCall, client);
+    return client;
+  }
+}
+
+module.exports = {
+  createClient: promisify('createClient'),
+  createSecureClient: promisify('createSecureServer'),
+  createServer: xmlrpc.createServer.bind(xmlrpc),
+  createSecureServer: xmlrpc.createSecureServer.bind(xmlrpc)
+};
