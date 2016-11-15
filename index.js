0998e23629ac5608bb649b03ddf4f854165c72dd
Richard Schaefer <rchrdschfr@gmail.com>
2016-11-15 22:06:53 +0800

    [BUGFIX] Change method to createSecureClient

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var Promise = require('bluebird');
 
 function promisify(methodName) {
   var method = xmlrpc[methodName];
-  
+
   return function() {
     var client = method.apply(xmlrpc, arguments);
     client.methodCall = Promise.promisify(client.methodCall, client);
@@ -13,7 +13,7 @@ function promisify(methodName) {
 
 module.exports = {
   createClient: promisify('createClient'),
-  createSecureClient: promisify('createSecureServer'),
+  createSecureClient: promisify('createSecureClient'),
   createServer: xmlrpc.createServer.bind(xmlrpc),
   createSecureServer: xmlrpc.createSecureServer.bind(xmlrpc)
 };
